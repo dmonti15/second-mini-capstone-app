d85efa93915042397892f51019e97a2337b9ed1b
Daniella Montinola <dmonti15@gmail.com>
2017-03-14 21:05:25 -0500

    html edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "plants#index"
   get "/plants" => "plants#index"
-  get "/plants/new/4/" => "plants#new"
+  get "/plants/new" => "plants#new"
   post "/plants" => "plants#create"
   get "/plants/:id" => "plants#show"
   get "/plants/:id/edit" => "plants#edit"
