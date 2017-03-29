785a2c97aaa3a994015010f671ff6dc0fc98052e
Daniella Montinola <dmonti15@gmail.com>
2017-03-28 19:53:32 -0500

    create action of carted products controller, save user_id, quantity and status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,20 @@
 class OrdersController < ApplicationController
 
   def create
-    plant_id = params[:plant_id]
-    plant = Plant.find_by(id: plant_id)
-    calculated_tax = plant.price * 0.09
-    order_total = plant.price + calculated_tax
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: plant_id,
-      quantity: params[:quantity],
-      subtotal: plant.price,
-      tax: calculated_tax,
-      total: order_total
-      )
-    order.save
-    redirect_to "/orders/#{order.id}"
+    # plant_id = params[:plant_id]
+    # plant = Plant.find_by(id: plant_id)
+    # calculated_tax = plant.price * 0.09
+    # order_total = plant.price + calculated_tax
+    # order = Order.new(
+    #   user_id: current_user.id,
+    #   product_id: plant_id,
+    #   quantity: params[:quantity],
+    #   subtotal: plant.price,
+    #   tax: calculated_tax,
+    #   total: order_total
+    #   )
+    # order.save
+    # redirect_to "/orders/#{order.id}"
   end
 
   def show
